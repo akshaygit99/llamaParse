2001a61f0c6a3b01b8756dddfc1ec0ec91b04b7e
akshaygit99 <76523662+akshaygit99@users.noreply.github.com>
2024-08-26 14:02:37 +0530

    Create demo.py

---
+++ ParsePDF/demo.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import nest_asyncio
+nest_asyncio.apply()
+
+
+from llama_parse import LlamaParse
+document=LlamaParse(result_type="markdown").load_data("./Invoice-9CB0F212-0001.pdf")
+
+from llama_index.core import VectorStoreIndex
+llama_parse_inex=VectorStoreIndex.from_documents(document)
+
+llama_parse_query_engine=llama_parse_inex.as_query_engine()
+
+print(llama_parse_query_engine.query
+      (
+          "What is the Bill To Address?"
+        
+      ))
+
+print(llama_parse_query_engine.query
+      (
+          "What are the API usage credits?'"
+        
+      ))
