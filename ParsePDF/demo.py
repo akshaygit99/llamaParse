d2ec3ff8a4eb3e9fa09fe7deddab2553f840caab
akshaygit99 <76523662+akshaygit99@users.noreply.github.com>
2024-11-28 20:54:52 +0530

    Update demo.py

--- ParsePDF/demo.py
+++ ParsePDF/demo.py
@@ -22,6 +22,9 @@ parser = LlamaParse(
     result_type="markdown"  # "markdown" and "text" are available
 )
 
+import os
+print(os.getcwd()) 
+
 # use SimpleDirectoryReader to parse our file
 file_extractor = {".pdf": parser}
 documents = SimpleDirectoryReader(input_files=['demo/test.pdf'], file_extractor=file_extractor).load_data()
